71bcb9137e4ce7e05c4ce0a4d0499f33b168282d
lentlx <lennylen4@gmail.com>
2022-06-20 10:20:44 +0200

    First commit

---
+++ workshop3A-groupe2/workshop3A_groupe2App.swift
@@ -0,0 +1,17 @@
+//
+//  workshop3A_groupe2App.swift
+//  workshop3A-groupe2
+//
+//  Created by Lenny THIEULEUX on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct workshop3A_groupe2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
